9551b498968b3e7bde7589c1d57388865bbb5709
EdMan1022 <EdMan1022@gmail.com>
2017-11-26 18:39:06 -0500

    Added logic to register the text_content blueprint with the app in `create_app.py`
    Signed-off-by: EdMan1022 <EdMan1022@gmail.com>

--- my_website/create_app.py
+++ my_website/create_app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from my_website.config import DevelopmentConfig
 from my_website.extensions import db
 from my_website.pages import page_blueprint
+from my_website.text_content import text_content
 
 
 def create_app(config=DevelopmentConfig()):
@@ -15,6 +16,7 @@ def create_app(config=DevelopmentConfig()):
     app = Flask(config.APP_NAME)
     app.config.from_object(config)
     app.register_blueprint(page_blueprint)
+    app.register_blueprint(text_content)
 
     db.init_app(app)
     db.app = app
